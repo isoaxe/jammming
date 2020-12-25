53ec860be2ed2403a6ae192f0cfb92a6bd2ab16a
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-12-25 14:11:54 +0700

    App.js: Refactor as Hooks
    Refactored the App component to utilise React Hooks.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar.js';
 import SearchResults from '../SearchResults/SearchResults.js';
@@ -6,77 +6,57 @@ import Playlist from '../Playlist/Playlist.js';
 import Spotify from '../../util/Spotify.js';
 
 
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      searchResults: [],
-      playlistTracks: [],
-      playlistName: 'New Playlist'
-    }
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-    this.updatePlaylistName = this.updatePlaylistName.bind(this);
-    this.savePlaylist = this.savePlaylist.bind(this);
-    this.search = this.search.bind(this);
-  }
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
 
-  addTrack(track) {
+  function addTrack(track) {
     // If the track is already on the playlist, do not save.
-    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+    if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     } else {
-      this.state.playlistTracks.push(track);
-      this.setState({
-        playlistTracks: this.state.playlistTracks
-      });
+      playlistTracks.push(track);
+      setPlaylistTracks(playlistTracks);
     }
   }
 
-  removeTrack(track) {
-    const index = this.state.playlistTracks.indexOf(track);
-    this.state.playlistTracks.splice(index, 1);
-    this.setState({
-      playlistTracks: this.state.playlistTracks
-    });
+  function removeTrack(track) {
+    const index = playlistTracks.indexOf(track);
+    playlistTracks.splice(index, 1);
+    setPlaylistTracks(playlistTracks);
   }
 
-  updatePlaylistName(name) {
-    this.setState({
-      playlistName: name
+  function updatePlaylistName(name) {
+    setPlaylistName(name);
+  }
+
+  function savePlaylist() {
+    const trackURIs = playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(playlistName, trackURIs).then(() => {
+      setPlaylistName('New Playlist');
+      setPlaylistTracks([]);
     })
   }
 
-  savePlaylist() {
-    const trackURIs = this.state.playlistTracks.map(track => track.uri);
-    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
-      this.setState({
-        playlistName: 'New Playlist',
-        playlistTracks: []
-      })
-    })
-  }
-
-  search(term) {
+  function search(term) {
     Spotify.search(term).then(searchResults => {
-      this.setState({searchResults: searchResults});
+      setSearchResults(searchResults);
     });
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-          <SearchBar onSearch={this.search} />
-          <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistTracks={this.state.playlistTracks} playlistName={this.state.playlistName} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
-          </div>
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar onSearch={search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist playlistTracks={playlistTracks} playlistName={playlistName} onRemove={removeTrack} onNameChange={updatePlaylistName} onSave={savePlaylist} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
