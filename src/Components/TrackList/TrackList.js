cd9d03fac7d68324720196de83d7e1c6f0902b5f
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-12-27 18:33:51 +0700

    TrackList: Refactor as Hooks
    Refactored the TrackList component to utilise React Hooks.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,14 +3,13 @@ import './TrackList.css';
 import Track from '../Track/Track.js';
 
 
-class TrackList extends React.Component {
-  render() {
-    return (
-      <div className="TrackList">
-        {this.props.tracks.map(track => <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />)}
-      </div>
-    );
-  }
+function TrackList(props) {
+  return (
+    <div className="TrackList">
+      {props.tracks.map(track => <Track track={track} key={track.id} onAdd={props.onAdd} onRemove={props.onRemove} isRemoval={props.isRemoval} />)}
+    </div>
+  );
 }
+
 
 export default TrackList;
