9ebd84bf6b0ca0ebde9a91fff1043559e3dd408f
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-02 11:13:04 +0700

    Playlist: Resolve bug that prevented the saving of playlist
    Was getting a 400 error that caused the POST request to Spotify to fail. The app was attempting to save a stale event rather than the event target value.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,9 +5,13 @@ import TrackList from '../TrackList/TrackList.js'
 
 function Playlist(props) {
 
+  function handleNameChange(e) {
+    props.onNameChange(e.target.value);
+  }
+
   return (
     <div className="Playlist">
-      <input defaultValue={"New Playlist"} onChange={props.onNameChange} />
+      <input defaultValue={"New Playlist"} onChange={handleNameChange} />
       <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true}  />
       <button className="Playlist-save" onClick={props.onSave}>SAVE TO SPOTIFY</button>
     </div>
