35a8a17354fcdc8cf243abd7fc3318ea5933b4b4
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-03 20:38:19 +0700

    Playlist Bug: Playlist name not resetting after save
    There was a bug whereby the name of the playlist as rendered on the screen did not reset to the specified value after saving a playlist. Instead, the name of the saved playlist persisted. This bug was resolved.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -11,8 +11,8 @@ function Playlist(props) {
 
   return (
     <div className="Playlist">
-      <input defaultValue={"New Playlist"} onChange={handleNameChange} />
-      <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true}  />
+      <input value={props.playlistName} onChange={handleNameChange} />
+      <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
       <button className="Playlist-save" onClick={props.onSave}>SAVE TO SPOTIFY</button>
     </div>
   );
