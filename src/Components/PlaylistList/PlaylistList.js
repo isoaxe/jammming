77ed7a82ac16390addaaa6b5d02821c7ffccc5c3
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-08 10:36:08 +0700

    PlaylistList: Add PlaylistList component
    Add skeleton code for PlaylistList component. This will be modelled on TrackList.

---
+++ src/Components/PlaylistList/PlaylistList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PlaylistList.css';
+
+
+function PlaylistList(props) {
+  return (
+    <div className="PlaylistList">
+
+    </div>
+  );
+}
+
+
+export default PlaylistList;
