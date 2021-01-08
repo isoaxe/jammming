51ab60704df3f63461169849b277d4a6a7d68790
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-08 10:46:14 +0700

    PlaylistItem: Add PlaylistItem component
    Add skeleton code for PlaylistItem component. This will be modelled on the Track component. PlaylistList and PlaylistItem will work together much like TrackList and Track.

---
+++ src/Components/PlaylistItem/PlaylistItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PlaylistItem.css';
+
+
+function PlaylistItem(props) {
+
+  return (
+    <div className="Playlist">
+      <div className="Playlist-information">
+      </div>
+    </div>
+  );
+}
+
+
+export default PlaylistItem;
