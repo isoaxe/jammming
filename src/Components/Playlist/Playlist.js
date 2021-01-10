644bade2da099492817a2d10fc527653fdf49256
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-10 11:51:28 +0700

    Playlist: Write generic message function
    Write function that displays a message where the text and color are specified as parameters. This replaces the hard-coded message that was previously used.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,6 +10,11 @@ function Playlist(props) {
     props.onNameChange(e.target.value);
   }
 
+  function message(text, color) {
+    setTimeout(() => document.getElementById("Message").style.color = color);
+    return <p id="Message">{text}</p>
+  }
+
   function displayMessage() {
     if (props.displayMsg) {
       document.getElementById("Message").style.visibility = "visible";
@@ -25,7 +30,7 @@ function Playlist(props) {
       <input value={props.playlistName} onChange={handleNameChange} />
       <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
       <button className="Playlist-button" onClick={props.onSave}>SAVE TO SPOTIFY</button>
-      <p id="Message">Playlist saved!</p>
+      {message('Playlist saved!', '#228B22')}
       <button className="Playlist-button" onClick={props.onRetrieve}>RETRIEVE PLAYLISTS</button>
       <PlaylistList playlists={props.playlists} delete={props.delete} />
     </div>
