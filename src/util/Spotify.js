6dd8bb5ae328a78e0ff858a3430ef70908c41c8a
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-17 12:42:36 +0700

    Spotify: Multiple tracks for deleteTracks function
    Renamed deleteTrack as deleteTracks since it will handle multiple tracks rather than a single track. Also changed the API request to this end.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -142,13 +142,17 @@ const Spotify = {
     })
   },
 
-  deleteTrack(trackURI, playlistId) {
+  deleteTracks(trackURIs, playlistId) {
     const accessToken = Spotify.getAccessToken();
     const headers = {Authorization: `Bearer ${accessToken}`};
+    const formattedTrackURIs = [];
+    for (let i=0; i<trackURIs.length; i++) {
+      formattedTrackURIs.push({uri: trackURIs[i]});
+    }
     return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
       method: 'DELETE',
       headers: headers,
-      body: JSON.stringify({tracks: [{uri: trackURI}]})
+      body: JSON.stringify({tracks: formattedTrackURIs})
     })
   },
 
